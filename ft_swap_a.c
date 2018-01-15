8b5e4d88ba64ddb311d7b7fe8c116d67667aafc2
Sigabort42 <elbenkrizi@gmail.com>
2018-01-15 03:23:22 +0100

    push_swap

--- ft_swap_a.c
+++ ft_swap_a.c
@@ -4,7 +4,6 @@ void	ft_swap_a(t_vari *var)
 {
 	int	d;
 
-	ft_putstr("\nswap_a\n");
 	if (!var->last_a || !var->last_a->prev)
 		return ;
 	d = var->last_a->nb;
--- ft_swap_b.c
+++ ft_swap_b.c
@@ -4,12 +4,8 @@ void	ft_swap_b(t_vari *var)
 {
 	int	d;
 
-	ft_putstr("\nswap_b\n");
 	if (!var->last_b || !var->last_b->prev)
-	{
-		ft_putstr("\nswap_b\n");
 		return ;
-	}
 	d = var->last_b->nb;
 	var->last_b->nb = var->last_b->prev->nb;
 	var->last_b->prev->nb = d;
