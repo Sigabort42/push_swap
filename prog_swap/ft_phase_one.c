ff4dbbf46b1c5c45356728ffd8b3df49c3f95754
Elharag BENKRIZI <elbenkri@e1r2p1.42.fr>
2018-07-24 06:09:21 +0200

    push_swap

--- prog_swap/ft_phase_one.c
+++ prog_swap/ft_phase_one.c
@@ -6,64 +6,42 @@
 /*   By: elbenkri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/21 21:22:43 by elbenkri          #+#    #+#             */
-/*   Updated: 2018/07/21 21:22:44 by elbenkri         ###   ########.fr       */
+/*   Updated: 2018/07/24 06:07:27 by elbenkri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-void	ft_phase_one(t_vari *var)
+static void	ft_exec(t_vari *var, int cmd, char *c_cmd)
 {
-	int	nb;
+	var->count++;
+	var->tab_func[cmd].f(var);
+	ft_putendl(c_cmd);
+}
 
+void	ft_phase_one2(t_vari *var)
+{
+	ft_exec(var, RRA_CMD, RRA);
+	ft_exec(var, SA_CMD, SA);
+	ft_exec(var, RA_CMD, RA);
+	ft_exec(var, RA_CMD, RA);
+}
+
+void		ft_phase_one(t_vari *var)
+{
+	int		nb;
+	int		div;
+
+	div = (ft_count_pile(var->last_a) / 3);
 	nb = var->first_a->nb;
-	while (var->last_a && (ft_count_pile(var->last_a) > 2 && var->last_a->nb != nb))
+	while (var->last_a && (ft_count_pile(var->last_a) > 2) && var->last_a->nb != nb)
 	{
 		if (var->last_a && var->last_a->nb > var->first_a->nb)
-		{
-			var->count++;
-			var->tab_func[RA_CMD].f(var);
-			ft_putendl(RA);
-		}
-		else if (var->last_a && var->last_a->nb < var->first_a->nb &&
+			ft_exec(var, RA_CMD, RA);
+		if (var->last_a && var->last_a->nb < var->first_a->nb &&
 		var->last_a->nb > var->first_a->next->nb)
-		{
-			var->count++;
-			var->tab_func[RRA_CMD].f(var);
-			ft_putendl(RRA);
-			var->count++;
-			var->tab_func[SA_CMD].f(var);
-			ft_putendl(SA);
-			var->count++;
-			var->tab_func[RA_CMD].f(var);
-			ft_putendl(RA);
-			var->count++;
-			var->tab_func[RA_CMD].f(var);
-			ft_putendl(RA);
-		}
+			ft_phase_one2(var);
 		else
-		{
-			var->count++;
-			var->tab_func[PB_CMD].f(var);
-			ft_putendl(PB);
-		}
-/*		while (var->last_b && var->last_b->nb < var->first_b->nb)
-		{
-			var->tab_func[RR_CMD].f(var);
-			ft_putendl(RR);
-			var->count++;
-			if (var->last_b->prev && var->last_b->nb < var->last_b->prev->nb)
-			{
-				var->tab_func[SB_CMD].f(var);
-				ft_putendl(SB);
-				var->count++;
-			}
-		}
-		if (var->last_b && var->last_b->prev && var->last_b->nb < var->last_b->prev->nb)
-		{
-			var->tab_func[SB_CMD].f(var);
-			ft_putendl(SB);
-			var->count++;
-		}
-*/	}
+			ft_exec(var, PB_CMD, PB);
+	}
 }
--- prog_swap/ft_phase_two.c
+++ prog_swap/ft_phase_two.c
@@ -6,7 +6,7 @@
 /*   By: elbenkri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/21 21:22:51 by elbenkri          #+#    #+#             */
-/*   Updated: 2018/07/21 21:22:52 by elbenkri         ###   ########.fr       */
+/*   Updated: 2018/07/24 06:07:42 by elbenkri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ static void	ft_exec(t_vari *var, char *name_cmd, int cmd)
 
 void		ft_phase_two(t_vari *var)
 {
-	int	tmp;
-	int	f;
+	int		tmp;
+	int		f;
 
 	f = 0;
 	if (var->last_a && var->last_a->prev && var->last_a->nb > var->last_a->prev->nb)
@@ -49,7 +49,7 @@ void		ft_phase_two(t_vari *var)
 			{
 				ft_exec(var, PA, PA_CMD);
 				ft_exec(var, RA, RA_CMD);
-			}			
+			}
 			if (var->last_b && var->last_a->nb < var->last_b->nb)
 			{
 				while (var->last_b && var->last_a->nb < var->last_b->nb)
