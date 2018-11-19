2f5933fc5e94a3d45395868b8b40d7ded62f7dd9
Sigabort42 <elbenkrizi@gmail.com>
2018-11-19 05:36:54 +0100

    news version

--- prog_swap/ft_phase_one.c
+++ prog_swap/ft_phase_one.c
@@ -17,27 +17,27 @@ int	ft_count_node(t_vari *var)
 
 void	ft_phase_one(t_vari *var)
 {
-	int	n;
-	int	m;
-	int	m2;
+	float	n;
+	float	m;
+	float	m2;
 	int	n_b;
 	t_lst	*tmp;
 
 	tmp = var->last_a;
 	n_b = 0;
-	n = ft_count_node(var);
+	n = (float)ft_count_node(var);
 	if (n > 150)
-		m = n / 20;
+		m = n / 14;
 	else if (n > 50)
-		m = n / 10;
+		m = n / 7;
 	else
-		m = n / 4;
+		m = n / 5;
 	m2 = m;
 	while (tmp->prev)
 	{
-		while (n_b < m2 - 1)
+		while (n_b < (int)m2 - 1)
 		{
-			if (tmp->nb <= m2 && tmp->nb != n && ++n_b)
+			if (tmp->nb <= (int)m2 && tmp->nb != n && ++n_b)
 				ft_exec(var, PB, PB_CMD);
 			else
 				ft_exec(var, RA, RA_CMD);
