dd47019b9e73f82f398a7430f2b4fc3bd2ba9c92
Sigabort42 <elbenkrizi@gmail.com>
2018-11-22 22:56:28 +0100

    news version

--- prog_swap/ft_phase_one.c
+++ prog_swap/ft_phase_one.c
@@ -17,27 +17,27 @@ int	ft_count_node(t_vari *var)
 
 void	ft_phase_one(t_vari *var)
 {
-	float	n;
-	float	m;
-	float	m2;
+	int	n;
+	int	m;
+	int	m2;
 	int	n_b;
 	t_lst	*tmp;
 
 	tmp = var->last_a;
 	n_b = 0;
-	n = (float)ft_count_node(var);
+	n = ft_count_node(var);
 	if (n > 150)
-		m = n / 14;
+		m = n / 20;
 	else if (n > 50)
-		m = n / 7;
+		m = n / 10;
 	else
-		m = n / 5;
+		m = n / 4;
 	m2 = m;
 	while (tmp->prev)
 	{
-		while (n_b < (int)m2 - 1)
+		while (n_b < m2 - 1)
 		{
-			if (tmp->nb <= (int)m2 && tmp->nb != n && ++n_b)
+			if (tmp->nb <= m2 && tmp->nb != n && ++n_b)
 				ft_exec(var, PB, PB_CMD);
 			else
 				ft_exec(var, RA, RA_CMD);
@@ -46,3 +46,60 @@ void	ft_phase_one(t_vari *var)
 		m2 += m;
 	}
 }
+
+/*void	ft_phase_one(t_vari *var)
+{
+	int	nb;
+
+	nb = var->first_a->nb;
+	while (var->last_a && (ft_count_pile(var->last_a) > 2 && var->last_a->nb != nb))
+	{
+		if (var->last_a && var->last_a->nb > var->first_a->nb)
+		{
+			var->count++;
+			var->tab_func[RA_CMD].f(var);
+			ft_putendl(RA);
+		}
+		else if (var->last_a && var->last_a->nb < var->first_a->nb &&
+		var->last_a->nb > var->first_a->next->nb)
+		{
+			var->count++;
+			var->tab_func[RRA_CMD].f(var);
+			ft_putendl(RRA);
+			var->count++;
+			var->tab_func[SA_CMD].f(var);
+			ft_putendl(SA);
+			var->count++;
+			var->tab_func[RA_CMD].f(var);
+			ft_putendl(RA);
+			var->count++;
+			var->tab_func[RA_CMD].f(var);
+			ft_putendl(RA);
+		}
+		else
+		{
+			var->count++;
+			var->tab_func[PB_CMD].f(var);
+			ft_putendl(PB);
+		}
+		while (var->last_b && var->last_b->nb < var->first_b->nb)
+		{
+			var->tab_func[RR_CMD].f(var);
+			ft_putendl(RR);
+			var->count++;
+			if (var->last_b->prev && var->last_b->nb < var->last_b->prev->nb)
+			{
+				var->tab_func[SB_CMD].f(var);
+				ft_putendl(SB);
+				var->count++;
+			}
+		}
+		if (var->last_b && var->last_b->prev && var->last_b->nb < var->last_b->prev->nb)
+		{
+			var->tab_func[SB_CMD].f(var);
+			ft_putendl(SB);
+			var->count++;
+		}
+	}
+}
+*/
